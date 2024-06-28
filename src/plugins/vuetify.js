92fd9dc319877c950c8287bad60042f71dfa6abe
Suvarnasrayshi <suvarnasrayshi.sinha.2024@gmail.com>
2024-06-28 12:42:16 +0530

    d

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,3 +8,57 @@ import { createVuetify } from 'vuetify'
 export default createVuetify(
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 )
+
+
+
+
+
+
+
+
+
+
+async fetchCart({ commit }) {
+  try {
+    const response = await fetch('https://fakestoreapi.com/carts');
+    const cart = await response.json();
+    // console.log("cart action details:", cart);
+   await Promise.all(cart.map(async (cart) => {
+      // console.log("cartdata", cart.products)
+    await Promise.all(cart.products.map(async (products) => {
+        // console.log("cartdataiddata", products.productId)
+        const response = await fetch(`https://fakestoreapi.com/products/${products.productId}`)
+        const cartdata = await response.json()
+      
+        commit('SET_CART', { cart: cartdata });
+      }))
+    }))
+  } catch (error) {
+    console.error('error in fetching of the cart details', error)
+  }
+}
+
+}
+
+<template>
+    <h2>Cart Items</h2>
+
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const cartItems = computed(() => {
+  console.log("cart vue data",store.state.product.cart);
+  return store.state.product.cart;
+});
+console.log(cartItems);
+onMounted(() => {
+  store.dispatch('fetchCart');
+});
+</script>
+
+
